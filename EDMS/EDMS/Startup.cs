cfc7f1e17c99b37e590929d3a68afc5541699093
Waseem Hijazi <waseemhijazi94@gmail.com>
2016-11-20 18:24:57 -0500

    added application to empty repository; first git bash commit

---
+++ EDMS/EDMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EDMS.Startup))]
+namespace EDMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
