abd14ec9bc04d5d9c22ef10633482754d68e3f4c
waseem <waseemhijazi94@gmail.com>
2016-11-27 18:42:50 -0500

    Added the Organization Model: this modelis satisfactory for the prototype

---
+++ EDMS/EDMS/Models/Organization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDMS.Models
+{
+    public class Organization
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string FundingAgencies { get; set; }
+        public decimal FundingAmount { get; set; }
+        public string YearFounded { get; set; }
+        public long NumberOfEmployees { get; set; }
+        public bool isVisible { get; set; }
+        public string PartnerOrganizations { get; set; }
+
+    }
+}
