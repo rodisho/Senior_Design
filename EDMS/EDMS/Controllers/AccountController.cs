df31042aee22e528fa9c2234b942a034a2b835ec
Waseem Hijazi <waseemhijazi94@gmail.com>
2016-12-11 17:15:18 -0500

    fixed bug where first time user registering is treated as outside user until program reset

--- EDMS/EDMS/Controllers/AccountController.cs
+++ EDMS/EDMS/Controllers/AccountController.cs
@@ -155,8 +155,9 @@ namespace EDMS.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     var result1 = UserManager.AddToRole(user.Id, "User");
+                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
